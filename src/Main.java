5a8a212206dd3b4e2a28a32f803b41efac8a30cd
gladys5 <yumanji@live.com>
2023-02-24 03:41:16 -0700

    readme

--- src/Main.java
+++ src/Main.java
@@ -6,20 +6,28 @@ public class Main {
      String [] TiposDCambio =new String[3];
      TiposDCambio[0]="PesosADollar";
      TiposDCambio[1]="DollarAPesos";
-     TiposDCambio[2]="EusosADollar";
-        String tipoDivisa = JOptionPane.showInputDialog(null,"ingresa el dipo de convercion", TiposDCambio);
+     TiposDCambio[2]="EurosADollar";
+
+//Tipo de cambio tiene que resivir todos los string dinamicos
+        String tipoDivisa = JOptionPane.showInputDialog(null,TiposDCambio);
         double numero = Double.parseDouble(JOptionPane.showInputDialog("Ingrese la cantidad de dinero que desea convertir \n "));
         Divisa divisa = DivisaFactory.crearDivisa(TipoDivisa.DollarAPesos);
         DollarAPesos dollarAPesos = (DollarAPesos) divisa;
 
         dollarAPesos.setDollar(numero);
-        System.out.println(divisa.calculaDivisa());
+         JOptionPane.showInputDialog(null,"El resultado es: ",divisa.calculaDivisa());
 
+/*
         divisa = DivisaFactory.crearDivisa(TipoDivisa.PesosADollar);
         PesosADollar pesosADollar =(PesosADollar) divisa;
         pesosADollar.setPesos(numero);
-        System.out.println(divisa.calculaDivisa());
+        JOptionPane.showInputDialog(null,"El resultado es: ",divisa.calculaDivisa());
+*/
 
+
+
+ //requiero dinamizar variables para reducir codigo ,lo primero que se me ocurre es crear un array y que este sea iterado hasta encontrar el valor pasado por el usuario
+        // con este valor cambiar la clase pasada a DivisaFactory.crearDivisa
     }
 
 
