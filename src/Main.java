2181d8b4c66325334afa25d0911ab05dbee80c6c
gladys5 <yumanji@live.com>
2023-02-25 22:55:58 -0700

    try catch

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,13 @@ import javax.swing.JOptionPane;
 
 
 public class Main {
+
+
+
+
+
+
+
     public static void main(String[] args) {
      String [] TiposDCambio =new String[3];
      TiposDCambio[0]="PesosADollar";
@@ -11,24 +18,33 @@ public class Main {
 //Tipo de cambio tiene que resivir todos los string dinamicos
         String tipoDivisa = JOptionPane.showInputDialog(null,TiposDCambio);
         double numero = Double.parseDouble(JOptionPane.showInputDialog("Ingrese la cantidad de dinero que desea convertir \n "));
-        Divisa divisa = DivisaFactory.crearDivisa(TipoDivisa.DollarAPesos);
-        DollarAPesos dollarAPesos = (DollarAPesos) divisa;
 
-        dollarAPesos.setDollar(numero);
-         JOptionPane.showInputDialog(null,"El resultado es: ",divisa.calculaDivisa());
 
-/*
-        divisa = DivisaFactory.crearDivisa(TipoDivisa.PesosADollar);
+try {
+    Divisa divisa = DivisaFactory.crearDivisa(TipoDivisa.DollarAPesos);
+    DollarAPesos dollarAPesos = (DollarAPesos) divisa;
+
+    dollarAPesos.setDollar(numero);
+
+    JOptionPane.showInputDialog(null,"El resultado es: ",divisa.calculaDivisa());
+}catch (NumberFormatException  | NullPointerException ex){
+    JOptionPane.showConfirmDialog(null,"Tiene que ser un numero \n");
+    ex.printStackTrace();
+}
+
+      /* divisa = DivisaFactory.crearDivisa(TipoDivisa.PesosADollar);
         PesosADollar pesosADollar =(PesosADollar) divisa;
         pesosADollar.setPesos(numero);
-        JOptionPane.showInputDialog(null,"El resultado es: ",divisa.calculaDivisa());
-*/
+        JOptionPane.showInputDialog(null,"El resultado es: ",divisa.calculaDivisa());*/
 
 
 
- //requiero dinamizar variables para reducir codigo ,lo primero que se me ocurre es crear un array y que este sea iterado hasta encontrar el valor pasado por el usuario
+        //requiero dinamizar variables para reducir codigo ,lo primero que se me ocurre es crear un array y que este sea iterado hasta encontrar el valor pasado por el usuario
         // con este valor cambiar la clase pasada a DivisaFactory.crearDivisa
+
+
     }
+
 
 
 
